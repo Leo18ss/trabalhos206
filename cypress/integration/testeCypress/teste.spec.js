472de86add5ac732ee04f12aa7431745f10e71f3
Leo18ss <leonardo1808ss@gmail.com>
2022-05-22 16:19:03 -0300

    trabalho finalizado

--- cypress/integration/testeCypress/teste.spec.js
+++ cypress/integration/testeCypress/teste.spec.js
@@ -1,29 +1,49 @@
-describe('Navegando no Mercado Livre', ()=>{
+describe('Acessando o Coin Market Cap', ()=>{
 
     beforeEach(()=> {
-        cy.visit("https://www.mercadolivre.com.br/")
-        cy.viewport(1200, 750)
-        tirarErroJS()
+        cy.viewport(1920, 1080)
+        cy.visit('https://coinmarketcap.com/pt-br/')
     })
 
-    it('Buscando uma placa de video', () => {
-        cy.get("input[name=as_word]").type("Placa de video {enter}")
+    it('Buscando uma moeda inexistente', () => {
+        cy.get('.sc-266vnq-1').click()
+        cy.get('.bzyaeu-3').type('LeoCoin {enter}')
     })
 
-    it('Voltando ao menu incial', () => {
-        cy.get("a[data-link-id=login]").click()
-        cy.get("a[.nav-logo]").click()
-    })
-
-    // it('Login na plataforma com sucesso!', () => {
-    //     //let userInfo = buscaProduto()
-    //     cy.visit("https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    it('Mudando idioma para ingles', () => {
+        cy.get('.ifkbzu').click()
+        cy.get('.YsYKS').eq(0).click()
         
-    // })
+    })
+
+    it('Tentando criar uma conta com e-mail e senha invalidos', () => { //teste de falso
+        cy.get('.naMuB').click()
+        cy.get('.dqGVRN').eq(0).type('leoteste')
+        cy.get('.dqGVRN').eq(1).type('159abc.')
+        cy.get('.dqGVRN').eq(2).type('159abc.') 
+        cy.get('.hSsFuv').click()
+    })
+
+    it('Tentando entrar em uma conta ate o captcha', () => {
+        cy.get('.DnSnS').click()
+        cy.get('.dqGVRN').eq(0).type('leoteste@inatel.br')
+        cy.get('.dqGVRN').eq(1).type('159aBc..')
+        cy.get('.hSsFuv').click()
+    })
+
+    it('Entrando na pagina da moeda Trending 0 atual', () => {
+        cy.get('.iworPT').eq(0).click()
+    })
+    
+    it('Buscando a BitCoin em Dolar', () => {
+        buscandoBitCoin()
+    })
+    
 })
 
-function tirarErroJS() {
-    cy.on('uncaught:exception', ()=> {
-        return false
-    })
+function buscandoBitCoin() {
+    cy.get('.sc-266vnq-1').click()
+    cy.get('.bzyaeu-3').type('BitCoin {enter}')
+    cy.get('.dUQeWc').click()
+    cy.get('.gqwalg').eq(0).click()
 }
